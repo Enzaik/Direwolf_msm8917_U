caa184a0f4d43000a7942a28aeb057b613a7e498
Malpa73 <kmktoczek@gmail.com>
2019-01-07 18:13:08 +0100

    disable debug
    (cherry picked from commit 9e6830c25ca906237d2a82ca2a935a7dbdcd1cb0)
    Signed-off-by: Enzaik <imernestodiaz@gmail.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -490,7 +490,7 @@ static int _cpu_up(unsigned int cpu, int tasks_frozen)
 	ret = __cpu_notify(CPU_UP_PREPARE | mod, hcpu, -1, &nr_calls);
 	if (ret) {
 		nr_calls--;
-		pr_warn("%s: attempt to bring up CPU %u failed\n",
+		pr_debug("%s: attempt to bring up CPU %u failed\n",
 			__func__, cpu);
 		goto out_notify;
 	}
